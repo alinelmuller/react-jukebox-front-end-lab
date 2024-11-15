57ffc9e34ae1331082837cc1285c0403198dd104
alinelmuller <alinelmuller@gmail.com>
2024-11-15 11:20:58 -0700

    init commit

---
+++ src/services/trackService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
+
+const index = async () => {
+    try {
+      const res = await axios.get(BASE_URL);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+export { index };
